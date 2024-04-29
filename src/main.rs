3746776dba074b35738a2a87217cc34664319a86
ahqrt <1217223113@qq.com>
2024-04-29 16:43:25 +0800

    feat: add process csv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 // rcli csv -i input.csv -o output.json --header -d ","
 
+use std::path::Path;
+
+use anyhow::Result;
 use clap::Parser;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Player {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
 
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, about = "A simple CLI tool for CSV processing", long_about = None)]
@@ -17,10 +36,10 @@ enum SubCommand {
 
 #[derive(Debug, Parser)]
 struct CsvOpts {
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = verify_input_file)]
     input: String,
 
-    #[arg(short, long, default_value = "output.json")]
+    #[arg(short, long, default_value = "output.json")] // default_value => "output.json".into()
     output: String,
 
     #[arg(short, long, default_value_t = ',')]
@@ -30,8 +49,28 @@ struct CsvOpts {
     header: bool,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // parse opts from command line
     let opts = Opts::parse();
-    print!("{:?}", opts)
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = Reader::from_path(opts.input)?;
+
+            let records = reader
+                .deserialize()
+                .map(|record| record.unwrap())
+                .collect::<Vec<Player>>();
+            println!("{:?}", records);
+        }
+    }
+    Ok(())
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File not found")
+    }
 }
