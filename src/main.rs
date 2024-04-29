f5e449c564ed91d9c0e144e6e833e3fdcbf8ba3a
ahqrt <1217223113@qq.com>
2024-04-29 17:09:18 +0800

    refactor: refactor process into files

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,15 @@
 // rcli csv -i input.csv -o output.json --header -d ","
 
-use std::path::Path;
-
-use anyhow::Result;
 use clap::Parser;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
-
-#[derive(Debug, Parser)]
-#[command(name = "rcli", version, about = "A simple CLI tool for CSV processing", long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or Convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")] // default_value => "output.json".into()
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
+use rcli::{process_csv, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     // parse opts from command line
     let opts = Opts::parse();
 
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-
-            let records = reader
-                .deserialize()
-                .map(|record| record.unwrap())
-                .collect::<Vec<Player>>();
-            println!("{:?}", records);
-        }
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
     Ok(())
-}
-
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File not found")
-    }
 }
---
+++ src/process.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+use anyhow::Result;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Player {
+    name: String,
+    position: String,
+    nationality: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+pub fn process_csv(input: &str, output: &str) -> Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let records = reader
+        .deserialize()
+        .map(|record| record.unwrap())
+        .collect::<Vec<Player>>();
+
+    let json = serde_json::to_string_pretty(&records)?;
+    fs::write(output, json)?;
+    Ok(())
+}
